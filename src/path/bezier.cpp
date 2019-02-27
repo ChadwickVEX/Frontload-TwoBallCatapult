b901398ff79b764f1daf9a6850a842c280fef96e
Manas Shah <manas@shahparivar.info>
2019-02-26 20:51:19 -0800

    confirmed working bezier curves

--- src/path/bezier.cpp
+++ src/path/bezier.cpp
@@ -60,76 +60,27 @@ Point Bezier::pointAt(int T)
 {
     Point point = {0 * okapi::inch, 0 * okapi::inch};
 
-    int n = points.size();
+    int n = points.size() - 1;
 
-    int sumX = 0;
-    int sumY = 0;
+    double sumX = 0;
+    double sumY = 0;
 
-    int lilT = (lilT > resolution) ? resolution : T;
+    int lilT = (T > resolution) ? resolution : T;
     double sT = (double)lilT / (double)resolution; // scaled t
 
 
     for (int i = 0; i <= n; i++) {
-        currX = points[i].x;
-        currY = points[i].y;
+        double currX = points[i].x.convert(okapi::inch);
+        double currY = points[i].y.convert(okapi::inch);
 
         sumX += combination(n, i) * std::pow(1 - sT, n - i) * std::pow(sT, i) * currX;
-        sumY += combination(n, i) * std::pow(1 - St, n - i) * std::pow(sT, i) * currX;
+        sumY += combination(n, i) * std::pow(1 - sT, n - i) * std::pow(sT, i) * currY;
     }
 
-    point.x = sumX;
-    point.y = sumY;
+    point.x = sumX * okapi::inch;
+    point.y = sumY * okapi::inch;
     point.t = lilT;
 
     return point;
-
-    // int rowIndex = points.size() - 1;
-    // std::vector<int> row;
-    // row.push_back(1);
-    // if (!(rowIndex == 0))
-    //     row.push_back(1);
-
-    // std::vector<int> result;
-    // for (int j = 2; j <= rowIndex; j++)
-    // {
-    //     result.clear();
-    //     result.push_back(1);
-    //     for (int i = 1; i <= j - 1; i++)
-    //     {
-    //         result.push_back(row[i - 1] + row[i]);
-    //     }
-    //     result.push_back(1);
-    //     row = result;
-    // }
-
-    // std::vector<int> coefficients = row;
-
-    // Point point = {0 * okapi::inch, 0 * okapi::inch};
-
-    // int lilT = (lilT > resolution) ? resolution : T;
-
-    // double sT = (double)lilT / (double)resolution; // scaled t
-
-    // double sumX = 0.0;
-    // double sumY = 0.0;
-
-    // for (int i = 0; i < points.size(); i++)
-    // {
-    //     Point currPoint = points[i];
-    //     for (int j = points.size() - 1; j >= 0; j--)
-    //     {
-    //         double oneTFactor = std::pow((1 - sT), j);
-    //         double tFactor = std::pow(sT, points.size() - 1 - j);
-
-    //         sumX += coefficients[i] * oneTFactor * tFactor * currPoint.x.convert(okapi::inch);
-    //         sumY += coefficients[i] * oneTFactor * tFactor * currPoint.y.convert(okapi::inch);
-    //     }
-    // }
-
-    // point.x = sumX * okapi::inch;
-    // point.y = sumX * okapi::inch;
-    // point.t = T;
-
-    // return point;
 }
 } // namespace path
